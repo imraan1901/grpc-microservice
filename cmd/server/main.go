703ea9b3fce194afc96f90fb49eb8654031fa4a0
Imraan Iqbal <imraan1901@gmail.com>
2023-05-23 09:43:00 -0700

    Added database layer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/imraan1901/grpc-microservice/internal/db"
+	"github.com/imraan1901/grpc-microservice/internal/rocket"
+)
 
 func run () error {
 	// responsible for initializing and 
 	// starting our gRPC server
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+	_ = rocket.New(rocketStore)
+
 	return nil
 }
 
